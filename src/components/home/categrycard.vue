c9a2160072c0703b9b9cc420b53856f9d2e0016b
andrew0samir <andrewsamir317@gmail.com>
2025-03-04 20:50:32 +0200

    added quizzes and categories pages to routes and navbar

--- src/components/home/categrycard.vue
+++ src/components/home/categrycard.vue
@@ -1,36 +1,36 @@
 <template>
-  <div class="bg-white p-6 rounded-lg shadow-md">
-    <!-- Icon -->
-    <div class="bg-teal-100 p-3 w-12 h-12 mx-auto rounded-md flex items-center justify-center">
-      <i :class="icon" class="text-teal-700 text-xl">{{ icon}}</i> 
-    </div>
-    <h3 class="font-semibold text-lg text-gray-800 mt-4">{{ title }}</h3>
-    <p class="text-gray-600 mt-2 text-sm">
-      {{ description }}
-    </p>
+    <div class="bg-white p-6 rounded-lg shadow-md">
+        <!-- Icon -->
+        <div class="bg-teal-100 p-3 w-12 h-12 mx-auto rounded-md flex items-center justify-center">
+            <i :class="icon" class="text-teal-700 text-xl">{{ icon }}</i>
+        </div>
+        <h3 class="font-semibold text-lg text-gray-800 mt-4">{{ title }}</h3>
+        <p class="text-gray-600 mt-2 text-sm">
+            {{ description }}
+        </p>
         <button class="mt-4 bg-teal-700 text-white px-12 py-2 rounded-lg shadow-lg hover:bg-teal-900">
-      See More
-    </button>
-  </div>
+            See More
+        </button>
+    </div>
 </template>
 
 <script>
 export default {
-  name: "CategoryCard",  
-  props: {
-    icon: {
-        type: String,
-        required: true,
-        default:"icon",
+    name: "CategoryCard",
+    props: {
+        icon: {
+            type: String,
+            required: true,
+            default: "icon",
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
     },
-    title: {
-      type: String,
-      required: true,
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-  },
 };
 </script>
--- src/components/quiz/QuizCard.vue
+++ src/components/quiz/QuizCard.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="bg-white p-6 rounded-lg shadow-md">
+  <div class="bg-teal-500 p-6 rounded-lg shadow-md m-2">
     <!-- Icon -->
-    <div class="bg-teal-100 p-3 w-12 h-12 mx-auto rounded-md flex items-center justify-center">
+    <div class="bg-teal-100 p-3 mx-auto rounded-md flex items-center justify-center">
       <i :class="icon" class="text-teal-700 text-xl">{{ icon}}</i> 
     </div>
     <h3 class="font-semibold text-lg text-gray-800 mt-4">{{ title }}</h3>

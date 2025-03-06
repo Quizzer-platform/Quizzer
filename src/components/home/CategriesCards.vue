2d58d933bd1e2e9e4af4cfff1fe3bb45e46e8acf
Ethar-Mohamed <etharfouad08@gmail.com>
2025-03-06 04:37:09 +0200

    implementation to post data and fetch of category

---
+++ src/components/home/CategriesCards.vue
@@ -0,0 +1,38 @@
+
+<template>
+    <div class="mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+      <div v-for="(category, index) in categories"
+        :key="index" >
+      <div class="bg-white p-6 rounded-xl shadow-lg m-2 w-64 text-center">
+      <!-- Icon Section -->
+      <div class="bg-teal-100 p-3 w-12 h-12 mx-auto rounded-md flex items-center justify-center">
+        <span class="text-teal-700 text-lg font-semibold">{{ category.icon }}</span>
+      </div>
+  
+      <!-- Quiz Title -->
+      <h3 class="font-semibold text-lg text-gray-900 mt-4">{{ category.title }}</h3>
+  
+      <!-- Quiz Description -->
+      <p class="text-gray-600 mt-2 text-sm">
+        {{ category.description }}
+      </p>
+  
+      <!-- Button -->
+      <button 
+        class="mt-4 bg-teal-700 text-white px-5 py-2 rounded-lg shadow-md w-full hover:bg-teal-900 transition">
+        See More
+      </button>
+    </div></div>
+    </div>
+  </template>
+  
+  <script>
+export default {
+  props: {
+    categories: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
--- src/views/CategoryView.vue
+++ src/views/CategoryView.vue
@@ -1,20 +1,61 @@
 <template>
     <Navbar />
-    <search customClass="" />
-    <Category />
+    <search />
+    <div class="flex justify-center">
+    <CategriesCards :categories="categories" />
+    </div>
     <Footer />
 </template>
 <script>
 import Navbar from "../components/layout/Navbar.vue";
 import Footer from "../components/layout/Footer.vue";
-import Category from "../components/home/Categories.vue";
-import search from "../components/layout/Searchbar.vue"
+import CategriesCards from "../components/home/CategriesCards.vue";
+import search from "../components/layout/Searchbar.vue";
+
 export default {
     components: {
-        Category,
+      CategriesCards,
         Navbar,
         Footer,
         search
-    }
+    },
+
+  data() {
+    return {
+    
+      categories: [  
+      ]
+    };
+  },
+
+
+methods: {
+        loadCategories() {
+            fetch("https://quizzer-platform-default-rtdb.firebaseio.com/categories.json", {})
+                .then(response => {
+                    if (response.ok) {
+                        return response.json();
+                    }
+                }).then(data => {
+                    console.log('Data:', data);
+                    for (const id in data) {
+                        this.categories.push({ icon:data[id].icon, title: data[id].title, description: data[id].description });
+                    }
+                    
+                }).catch(error => {
+                    console.error('Error:', error);
+                    alert('An error occurred. Please try again later.');
+                });
+        },
+
+    },
+    mounted() {
+        this.loadCategories();
+    },
+
+
+
 }
 </script>
+
+    
